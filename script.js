a79ab83580215e4675087e317b18b0bf9b4791eb
adrioro <artistsvillage@me.com>
2019-02-11 18:07:56 -0500

    Commit #4

---
+++ script.js
@@ -0,0 +1,10 @@
+/* h1.onclick = function () {
+    console.log("Testing Javascript!");
+};
+*/
+
+function clickFunction() {
+    $("#caroucel").fadeOut(300, function () {});
+    $(".subtitle").html("Looking for a Pastry, a Cake, or a Dessert?");
+    $("#find").hide();
+};
