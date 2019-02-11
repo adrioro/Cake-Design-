c4221936800ed67320ca6507bfc0ea7b0f5c67ef
adrioro <artistsvillage@me.com>
2019-02-11 17:04:12 -0500

    Commit #3

---
+++ index.js
@@ -0,0 +1,16 @@
+/* h1.onclick = function () {
+    console.log("Testing Javascript!");
+};
+*/
+
+
+/*
+$('h1').clic(function () {
+    console.log("Testing Javascript!");
+})
+*/
+
+
+document.getElementById("title").addEventListener("click", function () {
+    alert("Javascript Works!");
+});
